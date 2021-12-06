2f4f0e81be1add73ea01ff2597b32635b834eda8
= <=>
2021-12-06 04:46:33 +0300

    404 page

--- js/animation.js
+++ js/animation.js
@@ -9,18 +9,24 @@ $(document).ready(function(){
 
 		if (entry.isIntersecting) {
 			entryObject.classList.add('to-animate');
-			console.log("added")
 			return;
 		}
 
 		entryObject.classList.remove('to-animate');
-		console.log("removed");
 		});
 	});
 
-	var wrapper1 = document.querySelector('.vscode-img');
-	var wrapper2 = document.querySelector('.vscode-header');
+	var wrapper1 = document.querySelector('.tab-img#vscode');
+	var wrapper2 = document.querySelector('.tab-header#vscode');
+	var wrapper3 = document.querySelector('.tab-img#xcode');
+	var wrapper4 = document.querySelector('.tab-header#xcode');
+	var wrapper5 = document.querySelector('.tab-img#jb');
+	var wrapper6 = document.querySelector('.tab-header#jb');
 
 	observer.observe(wrapper1);
 	observer.observe(wrapper2);
+	observer.observe(wrapper3);
+	observer.observe(wrapper4);
+	observer.observe(wrapper5);
+	observer.observe(wrapper6);
 });
