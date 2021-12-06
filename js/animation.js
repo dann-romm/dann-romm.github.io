d294f431c1a3d9d1b496dc2d1e5a0ed17df69c37
= <=>
2021-12-06 20:26:10 +0300

    vscode page

--- js/animation.js
+++ js/animation.js
@@ -16,17 +16,10 @@ $(document).ready(function(){
 		});
 	});
 
-	var wrapper1 = document.querySelector('.tab-img#vscode');
-	var wrapper2 = document.querySelector('.tab-header#vscode');
-	var wrapper3 = document.querySelector('.tab-img#xcode');
-	var wrapper4 = document.querySelector('.tab-header#xcode');
-	var wrapper5 = document.querySelector('.tab-img#jb');
-	var wrapper6 = document.querySelector('.tab-header#jb');
-
-	observer.observe(wrapper1);
-	observer.observe(wrapper2);
-	observer.observe(wrapper3);
-	observer.observe(wrapper4);
-	observer.observe(wrapper5);
-	observer.observe(wrapper6);
+	document.querySelectorAll('.tab-img').forEach( item => {
+		observer.observe(item);
+	});
+	document.querySelectorAll('.tab-header').forEach( item => {
+		observer.observe(item);
+	});
 });
